3141da39d85f00993ed169d542e28ee7acbe496c
Jonathan <Razordor@gmail.com>
2020-04-30 18:07:10 -0700

    Add files via upload

---
+++ SRC/lib.rs
@@ -0,0 +1,17 @@
+﻿//see Rust book for information on how to do unit testing:
+//https://doc.rust-lang.org/book/ch11-01-writing-tests.html
+
+#[cfg(test)]
+mod tests{
+	#[test]
+	fn example_test0(){
+		let x = 5;
+		assert_eq!(x, 5);
+	}
+
+	#[test]
+	fn example_test1(){
+		let x = "hello world";
+		assert_eq!(x, "ello world");
+	}
+}
